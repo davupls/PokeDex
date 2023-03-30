94c5be56285e7803d6ef4847be4341f42e877727
David Mclean <47851457+davupls@users.noreply.github.com>
2023-03-29 23:03:28 -0500

    - Add 2 column layout using LazyVGrid - Load List layout into Grid

--- pokemonPokeDex/Views/ContentView.swift
+++ pokemonPokeDex/Views/ContentView.swift
@@ -11,28 +11,40 @@ struct ContentView: View {
     
     @ObservedObject var viewModel = PokeDexVIEWModel()
     
+    private var twoColumnGrid = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
         ScrollView {
-            ForEach(viewModel.pokedexlist) { pokemon in
-                LazyVStack {
+            Text("PokeDex")
+                .font(.largeTitle)
+                .bold()
+            LazyVGrid(columns: twoColumnGrid, spacing: 10) {
+                ForEach(viewModel.pokedexlist) { pokemon in
                     
-                    Image("00" + String(pokemon.id))
-                    Text(pokemon.name["english"] ?? "")
-                        .font(.title2)
-                        .bold()
-                        .padding(.bottom)
-                    Text("00" + String(pokemon.id))
-                        .font(.footnote)
+                    VStack {
+                        Image("00" + String(pokemon.id))
+                        Text(pokemon.name["english"] ?? "")
+                            .font(.title2)
+                            .bold()
+                            .padding(.bottom)
+                        Text("00" + String(pokemon.id))
+                            .font(.footnote)
+                    }
+//                    .padding()
+                    
                 }
-                .frame(width: 220, height: 300)
+                
+                .frame(width: 180, height: 230)
                 .background(.blue)
                 .foregroundColor(.white)
                 .cornerRadius(20)
-                
             }
+            
         }
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
